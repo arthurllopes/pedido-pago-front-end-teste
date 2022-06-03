5a5f3e076441a1d5137c44e9096ca6abe1185761
Arthur Lopes <arthurllopes10@gmail.com>
2022-06-03 18:57:00 -0300

    fix deploy?

--- src/pages/colaborator/[id].tsx
+++ src/pages/colaborator/[id].tsx
@@ -7,7 +7,6 @@ import api from '../../services/api'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import ColaboratorDetailsBox from '../../components/ColaboratorDetailsBox'
 import Link from 'next/link'
-import { MainContainer } from './style'
 
 export type Agent = {
   id: number,
@@ -67,3 +66,48 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     }
   }
 }
+
+const MainContainer = styled.main`
+  display: flex;
+  max-width: 100vw;
+
+  .main {
+    padding: 0 14px;
+    padding-top: 40px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin: 0 auto;
+  }
+
+  .title {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-weight: 600;
+    font-size: 2rem;
+
+    @media (max-width: 800px) {
+      font-size: 1.8rem;
+    }
+    @media (max-width: 800px) {
+      font-size: 1.4rem;
+    } 
+  }
+  .back-btn {
+    width: 36px;
+    height: 36px; 
+    border-radius: 50%;
+    background-color: #EAEFED;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    margin-right: 1rem;
+
+    &:hover {
+      cursor: pointer;
+      filter: brightness(.8);
+    }
+  }
+`
